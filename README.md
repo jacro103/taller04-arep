3d8bffbf3c3658922752a7a26a8a42b9cc0b71db
jose <jose.correa-r@mail.escuelaing.edu.co>
2024-02-28 15:05:48 -0500

    terminado

--- README.md
+++ README.md
@@ -1,1 +1,167 @@
+# AREP-Taller-5
+En este taller se tuvo como objetivo realizar un servidor web utilizando Spark y que respondiera a ciertas peticiones y tambien se subiera la imagen del contenedor a Docker
 
+## Para ejecutar el programa
+
+Se puede hacer uso del comando git clone y usar la URL del repositorio:
+```
+https://github.com/jacro103/taller04-arep.git
+```
+
+## Prerequisitos
+
+Es necesario tener instalado maven para compilar y probar los test del programa, si no se tiene maven puede instalar [aqui](https://maven.apache.org/install.html).
+
+## Instalacion 
+
+Una vez clonado, se debe hacer uso del comando:
+
+```
+mvn package
+```
+
+Este comando compilara el programa y tambien ejecutara las pruebas. 
+
+## Despliegue del programa:
+
+Para ejecutar el programa se usa el comando:
+
+```
+java -cp "target/classes;target/dependency/*" com.mycompany.taller04.arep.SparkWebServer
+```
+Una vez ejecutado se debe acceder a traves de un buscador y con la direccion:
+```
+localhost:4567
+```
+
+## Descripcion del programa:
+
+### Diseño:
+
+En este programa se usa la clase SparkWebServer para realizar la implementacion de las respuestas a peticiones desde un cliente usando el framework Spark.
+
+### Implementacion:
+
+#### Desarrollo servidor:
+
+Para comenzar se realizaron los metodos para responder a las diferentes implementaciones que se requerian
+
+Aqui podemos ver las cuatro implementaciones requeridas:
+
+* Determinar el Seno de un valor.
+* Determinar el Coseno de un valor.
+* Determinar si una palabra es palindroma.
+* Deteminar la magnitud de un vector real de dos dimensiones.
+
+
+#### Construccion de imagenes en Docker:
+
+Para empezar se debe tener Docker Desktop instalado en el computador, y crearemos un archivo Dockerfile en la raiz del proyecto:<br>
+
+
+Ahora utilizaremos el siguiente comando para construir la imagen:<br>
+
+```
+docker build --tag dockersparkprimer .
+```
+
+
+Y ahora usando:
+
+```
+docker images
+```
+
+
+
+Ahora crearemos tres contenedores usando:
+
+```
+docker run -d -p 34000:6000 --name firstdockercontainer dockersparkprimer
+docker run -d -p 34001:6000 --name firstdockercontainer2 dockersparkprimer
+docker run -d -p 34002:6000 --name firstdockercontainer3 dockersparkprimer
+```
+
+
+Y usando el siguiente comando verificamos el estado de los contenedores:
+
+```
+docker ps
+```
+
+
+Y ahora accedemos a la siguiente url y verificamos el estado de los contenedores:
+
+```
+localhost:34000
+localhost:34001
+localhost:34002
+```
+
+
+
+Ahora crearemos un archivo docker-compose.yml en la raiz del directorio
+
+Y ejecutamos el siguiente comando:
+
+```
+docker-compose up -d
+```
+
+
+Usando el siguiente comando verificamos los servicios:
+
+```
+docker ps
+```
+
+
+
+Ahora subiremos la imagen a un repositorio en la web de [Docker](https://www.docker.com).<br>
+
+Para empezar crearemos un repositorio
+
+Y ahora crearemos una referencia local de la imagen con el siguiente comando:
+
+```
+docker tag dockersparkprimer jacro103/lab04taller
+```
+
+
+
+Ahora nos autenticamos en nuestra cuenta de docker y subimos la imagen al repositorio:
+
+```
+docker push juanpablodaza/arep-taller-5:latest
+```
+
+
+
+Y ahora viendo el repositorio podemos ver la imagen:<br>
+
+
+
+El repositorio donde esta subida es:
+
+```
+https://hub.docker.com/r/jacro103/lab04taller
+```
+
+Para correr la imagen podemos hacer el siguiente proceso:
+
+1. Hacemos ```docker pull jacro103/lab04taller``` para descargar la imagen desde el repositorio.
+
+2. Usamos ```docker ps``` para poder ver en que puerto esta escuchando el servidor
+
+
+
+
+## Construido con:
+
+* [Maven](https://maven.apache.org/) - Manejo de dependecias.
+
+* [Docker](https://www.docker.com) - Creacion de imagenes.
+
+## Autor
+
+* Juan Pablo Daza Pinzón
--- src/main/java/com/mycompany/taller04/arep/SparkWebServer.java
+++ src/main/java/com/mycompany/taller04/arep/SparkWebServer.java
@@ -1,48 +1,150 @@
 
 package com.mycompany.taller04.arep;
 
+
 import static spark.Spark.get;
 import static spark.Spark.port;
 
-
+import spark.Request;
+import spark.Response;
 
 public class SparkWebServer {
-    
-    public static void main(String... args){
-          port(getPort());
-          get("hello", (req,res) -> "Hello Docker!");
+
+    public static void main(String... args) {
+        port(getPort());
+        get("hello", (req, res) -> "Hello Docker!");
+        get("/", (req, res) -> getIndex() );
+        get("/sen", (req, res) -> Math.sin((Double.parseDouble(req.queryParams("value")))));
+        get("/cos", (req, res) -> Math.cos((Double.parseDouble(req.queryParams("value")))));
+        get("/palindromo", (req, res) -> {
+            String str = req.queryParams("value");
+            String rstr = "";
+            int strLength = str.length();
+            for (int i = (strLength - 1); i >=0; --i) {
+                rstr = rstr + str.charAt(i);
+              }
+          
+              if (str.toLowerCase().equals(rstr.toLowerCase())) {
+                return " Es palindromo.";
+              }
+              else {
+                return " No es palindromo.";
+              }
+
+        });
+        get("/magnitud", (req, res) -> {
+            try {
+                double num1 = Double.parseDouble(req.queryParams("valueA"));
+                double num2 = Double.parseDouble(req.queryParams("valueB"));
+                double magnitud = Math.sqrt((num1*num1)+(num2*num2));
+                return magnitud;
+            } catch (Exception e) {
+                return "Valores invalidos, por favor compruebelos y vuelva a dar click en enviar.";
+            }
+        });
     }
 
+    
     private static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
     }
-    
-    public double cos(double num){
-        double cos = Math.cos(num);
-        return cos;
+
+    public static String getIndex() {
+        String response = 
+                "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html\n>"
+                + "    <head\n>"
+                + "        <title>Taller 5</title\n>"
+                + "        <meta charset=\"UTF-8\"\n>"
+                + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "    </head>\n"
+                + "    <body>\n"
+                + "        <h1>Servidor con Spark</h1>\n"
+                + "        <h2>Funciones disponibles:</h2>\n"
+                + "        <form action=\"/sin\">\n"
+                + "            <label for=\"Sin\">Seno:</label><br>\n"
+                + "            <input type=\"text\" id=\"sin\" name=\"seno\" value=\"\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Enviar\" onclick=\"loadGetSinMsg()\">\n"
+                + "        </form>\n"
+                + "        <div id=\"getrespsinmsg\"></div>\n"
+                + "      <script>\n"
+                + "            function loadGetSinMsg() {\n"
+                + "                let nameVar = document.getElementById(\"sin\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespsinmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                } \n"
+                + "                xhttp.open(\"GET\", \"sen?value=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "      </script>\n"
+                + "        <form action=\"/cos\">\n"
+                + "            <label for=\"Cos\">Coseno:</label><br>\n"
+                + "            <input type=\"text\" id=\"cos\" name=\"cos\" value=\"\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Enviar\" onclick=\"loadGetCosMsg()\">\n"
+                + "        </form>\n"
+                + "        <div id=\"getrespcosmsg\"></div>\n"
+                + "      <script>\n"
+                + "            function loadGetCosMsg() {\n"
+                + "                let nameVar = document.getElementById(\"cos\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespcosmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                } \n"
+                + "                xhttp.open(\"GET\", \"cos?value=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "      </script>\n"
+                + "        <form action=\"/palindromo\">\n"
+                + "            <label for=\"Cos\">Palabra palindroma:</label><br>\n"
+                + "            <input type=\"text\" id=\"palindromo\" name=\"palindromo\" value=\"\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Enviar\" onclick=\"loadGetPalMsg()\">\n"
+                + "        </form>\n"
+                + "        <div id=\"getresppalmsg\"></div>\n"
+                + "      <script>\n"
+                + "            function loadGetPalMsg() {\n"
+                + "                let nameVar = document.getElementById(\"palindromo\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getresppalmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                } \n"
+                + "                xhttp.open(\"GET\", \"palindromo?value=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "      </script>\n"
+                + "        <form action=\"/magnitud\">\n"
+                + "            <label for=\"Magnitud\">Magnitud de dos dimensiones:</label><br>\n"
+                + "            <input type=\"text\" id=\"dimension1\" name=\"dimension1\" value=\"\"><br><br>\n"
+                + "            <input type=\"text\" id=\"dimension2\" name=\"dimension2\" value=\"\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Enviar\" onclick=\"loadGetMagMsg()\">\n"
+                + "        </form>\n"
+                + "        <div id=\"getrespmagmsg\"></div>\n"
+                + "      <script>\n"
+                + "            function loadGetMagMsg() {\n"
+                + "                let dim1 = document.getElementById(\"dimension1\").value;\n"
+                + "                let dim2 = document.getElementById(\"dimension2\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespmagmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                } \n"
+                + "                xhttp.open(\"GET\", \"magnitud?valueA=\" + dim1 + \"&valueB=\" + dim2);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "      </script>\n"
+                + "    </body>\n"
+                + "</html>\n";
+        return response;
     }
-    
-    public double sin(double num){
-        double sin = Math.sin(num);
-        return sin;
-    }
-    public double vec(double numA, double numB){
-        double a =numA * numA;
-        double b = numB * numB;
-        double c = a +b;
-        double d = Math.sqrt(c);
-        return d;
-    }
-    
-    public Boolean palindromo(String palabra){
-        Boolean palindromo = false;
-        
-        return palindromo;
-    }
-    
+
 }
+
 
 
